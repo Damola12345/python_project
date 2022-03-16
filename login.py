0500f0f353651ccee13d9cae6638519d33363c4f
Damola12345 <ajiboladamola789@gmail.com>
2022-03-16 22:10:13 +0100

    login sys

---
+++ login.py
@@ -0,0 +1,22 @@
+# python program
+# Build a simple login and sign up system
+
+# username
+username    = input('Enter username: ')
+# password
+password    = input('Enter password: ')
+
+print('Your account has been successfully created')
+print('Login now!')
+
+# username2
+username2 = input('Enter username: ')
+
+# password2
+password2 = input('Enter password: ')
+
+
+if username == username2 and password == password2:
+    print('Logged in successfully')
+else:
+    print('Invalid successfully')
